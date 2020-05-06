fb4466942c58c2ff47d0c35d47cdcd957a885f58
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-06 18:03:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,10 @@
 
 katz_deli = []
 
-# def line(array) 
-#   if array.length == 0 
-#     puts "The line is currently empty."
-#   else
-#     puts "The line is currently: " + array.each_with_index |person, index|
-#     "#{index + 1}. #{name} "
-# end
+def line(array) 
+  if array.length == 0 
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: " + array.each_with_index |person, index|
+    "#{index + 1}. #{name} "
+end
