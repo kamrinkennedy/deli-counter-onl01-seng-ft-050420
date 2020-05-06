f6dcbbe98babe2372d5d0fbf032caf35fc850f9c
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-06 18:15:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,10 +18,8 @@ def line(array)
   if array.length == 0 
     puts "The line is currently empty."
   else
-    current_line = "The line is currently:"
-    array.each_with_index do |customer, index|
-      current_line << " #{index + 1}. #{customer}"
+    puts "The lne is currently:" + array.each_with_index do |customer, index|
+      " #{index + 1}. #{name}"
     end
-    puts current_line
   end
 end
