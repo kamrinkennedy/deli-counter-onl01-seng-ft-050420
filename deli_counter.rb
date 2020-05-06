62ffdc34613c5e9d698d7c16646f833f15876f53
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-06 18:30:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,7 @@
 
 katz_deli = []
 
+# Method to display the current line, if there is one.
 def line(array) 
   if array.length == 0 
     puts "The line is currently empty."
@@ -14,7 +15,18 @@ def line(array)
   end
 end
 
+# Method to display a new customer's spot in the line.
 def take_a_number(array, name)
   array.push(name)
   puts "Welcome, #{name}. You are number #{array.length} in line."
+end
+
+# Method to display the next person in line and remove them from the front.
+def now_serving(array)
+  if array.length == 0 
+    puts "There is nobody waiting to be served."
+  else
+    puts "Currently serving: #{array.[0]}."
+    array.shift
+  end
 end
